c7cc1b9d00ffc1f89d7cfa7607153fd12ea7dab5
Pimporn <pimporn.kate@gmail.com>
2022-09-20 15:41:09 +0700

    add:answer Kate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Spinner } from '@chakra-ui/react'
+import { Input } from '@chakra-ui/react'
+import { Component } from 'react';
+import DisplayJohn from './components/DisplayJohn'
+import { JohnProfile } from './components/DisplayJohn';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+        <Button colorScheme='blue' size= 'xs'>Button</Button>
+        </div><div>      
+        <Button colorScheme='red' size='md'>Button</Button>
+        </div><div>
+<Button colorScheme='pink' size = 'lg'>Button</Button> 
+</div><div>
+        <Spinner  size = 'xs'/>
+        <Spinner color='red.500' size= 'md' />
+        <Spinner
+  thickness='4px'
+  speed='0.65s'
+  emptyColor='gray.200'
+  color='blue.500'
+  size='xl'/>
+ 
+  <Input placeholder='extra small size' size='xs' />
+  <Input placeholder='small size' size='sm' />
+  <Input placeholder='medium size' size='md' />
+  <Input placeholder='large size' size='lg' />
+  <DisplayJohn/>
+  <JohnProfile/>
+
+
+
+        
+</div>
     </div>
   );
 }
